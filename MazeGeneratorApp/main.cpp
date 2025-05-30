0e8acf028a775fa95828ecd23d91c469b30d7f13
caysun <133520803+caysun@users.noreply.github.com.>
2025-05-30 06:39:14 -0400

    Generated Path and Highlighted Path Cells Green in Grid

--- MazeGeneratorApp/main.cpp
+++ MazeGeneratorApp/main.cpp
@@ -5,22 +5,38 @@
 std::vector<std::pair<int, int>> dfs(std::pair<int, int> curCell, std::pair<int, int> endCell, 
     std::vector<std::pair<int, int>> &pathCells, std::vector<std::vector<bool>> &visited){
     
-    if(curCell == endCell) return pathCells;
+    if (curCell == endCell) {
+        pathCells.push_back(curCell);
+        return pathCells;
+    }
     pathCells.push_back(curCell);
     visited[curCell.first][curCell.second] = true;
     std::vector<int> dr = {0, -1, 0, 1};
     std::vector<int> dc = {-1, 0, 1, 0};
+
+    // Go in a random direction
+    std::vector<int> directions = {0, 1, 2, 3};
+
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    std::shuffle(directions.begin(), directions.end(), gen);
     for(int i=0; i<4; i++){
-        int nr = curCell.first + dr[i];
-        int nc = curCell.second + dc[i];
+        int nr = curCell.first + dr[directions[i]];
+        int nc = curCell.second + dc[directions[i]];
+        if(nr < 0 || nr > 9 || nc < 0 || nc > 9 || visited[nr][nc]) continue;
         std::vector<std::pair<int, int>> currentCells = dfs({nr, nc}, endCell, pathCells, visited);
-        if(currentCells[currentCells.size()-1].first == 9 && currentCells[currentCells.size()-1].second == 9) return currentCells;
+        if(currentCells[currentCells.size()-1] == endCell) return currentCells;
     }
     visited[curCell.first][curCell.second] = false;
+    pathCells.pop_back();
     return pathCells;
 }
 
 int main(){
+
+    // Initialize constant blocked cells
+    const int blocked = 15;
 
     // Create Window and define dimensions
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
@@ -40,35 +56,35 @@ int main(){
             sf::RectangleShape rect({gridWidth, gridHeight});
             rect.setOrigin(rect.getSize().x / 2.f, rect.getSize().y / 2.f);
             rect.setPosition(gridStartPosX + gridWidth*c, gridStartPosY + gridHeight*r);
-            rect.setFillColor(sf::Color::Black);
-            rect.setOutlineColor(sf::Color::White);
-            rect.setOutlineThickness(10.f);
             maze[r][c] = rect;
-            if( (r != 0 && c != 0) && (r != 9 && c != 9) ) gridCells[r*10 + c - 1] = {r, c};
+            if( (r != 0 && c != 0) || (r != 9 && c != 9) ) gridCells[(r*10 + c - 1)] = std::make_pair(r, c);
         }
     }
 
-    // Define Maze Solution with Inacessible Grid Blocks using depth-first-search (DFS)
-    std::vector<std::vector<bool>> visited(10, std::vector<bool>(10, false));
-
+    
+    // Seed Randomly
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    // Select random cells to be blocked
-    std::shuffle(gridCells.begin(), gridCells.end(), gen);
-    for(int i=0; i<8; i++) visited[gridCells[i].first][gridCells[i].second] = true;
+    std::vector<std::pair<int, int>> pathSolution;
+    do{
+        // Define Maze Solution with Inacessible Grid Blocks using depth-first-search (DFS)
+        std::vector<std::vector<bool>> visited(10, std::vector<bool>(10, false));
 
-    std::vector<std::pair<int, int>> emptyVector;
-    std::vector<std::pair<int, int>> pathSolution = dfs({0, 0}, {9, 9}, emptyVector, visited);
+        // Select random cells to be blocked
+        std::shuffle(gridCells.begin(), gridCells.end(), gen);
+        for(int i=0; i<blocked; i++) visited[gridCells[i].first][gridCells[i].second] = true;
+
+        // Attempt to find path solution
+        std::vector<std::pair<int, int>> emptyVector;
+        pathSolution = dfs({0, 0}, {9, 9}, emptyVector, visited);
+        
+    } while(pathSolution.back() != std::make_pair(9, 9));
 
     // Print out path solution
-    int grid[10][10];
+    std::vector<std::vector<int>> grid(10, std::vector<int>(10, 0));
     for(auto &x : pathSolution){
         grid[x.first][x.second] = 1;
-    }
-    for(auto &x : grid){
-        for(auto &y :x) std::cout << y;
-        std::cout << std::endl;
     }
 
     sf::Event event;
@@ -85,13 +101,23 @@ int main(){
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) window.close();
         }
 
-        // Clear the window with black color
+        // Clear the window with solid color
         window.clear(sf::Color::Blue);
-        
-        // Draw grid rectangles
-        for(auto &row : maze){
-            for(auto &grid : row){
-                window.draw(grid);
+
+        // Color rectangles in Path Green
+        for(int r=0; r<grid.size(); r++){
+            for(int c=0; c<grid[r].size(); c++){
+                if(grid[r][c] == 0){
+                    maze[r][c].setFillColor(sf::Color::Black);
+                    maze[r][c].setOutlineColor(sf::Color::White);
+                    maze[r][c].setOutlineThickness(10.f);
+                    window.draw(maze[r][c]);
+                    continue;
+                }
+                maze[r][c].setFillColor({0, 100, 0});
+                maze[r][c].setOutlineColor(sf::Color::White);
+                maze[r][c].setOutlineThickness(10.f);
+                window.draw(maze[r][c]);
             }
         }
 
